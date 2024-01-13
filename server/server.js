2235f3c2e31551a55ff1ead3ff90d9df10a08939
daevidvo <itsdavidvo@gmail.com>
2024-01-12 20:53:28 -0800

    worked more on server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
 const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+})
+
